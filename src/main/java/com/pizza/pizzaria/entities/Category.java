e8c3be0f9d02fa4c07483a90b6ccbcae3a680894
VitaoIF <victormelo1212@gmail.com>
2026-06-11 20:36:00 -0300

    feat: adiciona configuração para id do tipo sequence

--- src/main/java/com/pizza/pizzaria/entities/Category.java
+++ src/main/java/com/pizza/pizzaria/entities/Category.java
@@ -1,14 +1,26 @@
 package com.pizza.pizzaria.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "category")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
-    @SequenceGenerator(name = "seq", allocationSize = 50)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_seq")
+    @SequenceGenerator(
+            name = "category_seq",
+            sequenceName = "category_seq",
+            allocationSize = 1
+    )
     private Long id;
 
     private String name;
