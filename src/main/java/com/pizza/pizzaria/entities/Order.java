789f5cb7c82b2581e8daf37b8fd51142fc0ebe70
VitaoIF <victormelo1212@gmail.com>
2026-06-12 20:41:40 -0300

    feat: adiciona entidade order

---
+++ src/main/java/com/pizza/pizzaria/entities/Order.java
@@ -0,0 +1,46 @@
+package com.pizza.pizzaria.entities;
+
+import com.pizza.pizzaria.entities.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_seq")
+    @SequenceGenerator(
+            name = "order_seq",
+            sequenceName = "order_seq",
+            allocationSize = 1
+    )
+    private Long id;
+
+    private String name;
+
+    @Column(name = "table_number")
+    private Integer tableNumber;
+
+    private boolean draft;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
