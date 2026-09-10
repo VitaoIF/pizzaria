ca1a13c48c52b984bf77f086e1616daa56c9bcd4
VitaoIF <victormelo1212@gmail.com>
2026-09-09 22:41:58 -0300

    feat: Adiciona entidade OrderItem

---
+++ src/main/java/com/pizza/pizzaria/entities/OrderItem.java
@@ -0,0 +1,31 @@
+package com.pizza.pizzaria.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_item_seq")
+    @SequenceGenerator(
+            name = "order_item_seq",
+            sequenceName = "order_item_seq",
+            allocationSize = 1
+    )
+    private Long id;
+
+    private Integer quantity;
+    private BigDecimal unitPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
